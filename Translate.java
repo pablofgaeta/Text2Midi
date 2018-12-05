3a23aa18b01c71ff499e076a6f2ddd7e95ab5e69
pablofgaeta <37568237+pablofgaeta@users.noreply.github.com>
2018-12-04 17:08:55 -0800

    Update Translate.java

--- Translate.java
+++ Translate.java
@@ -1,3 +1,20 @@
+/*
+ * Pablo Gaeta
+ * Dec 4, 2018
+ * Translate.java
+ * 
+ * Description:
+ *  Writes an ascii representation of midi data to a file, mainOut, from an input text file, input.txt.
+ * 
+ *  Methods:
+ *  main            - Runs newSong method and records run time
+ *  readFile        - Reads the specified file and returns it as a string
+ *  append2midifile - Adds a new line to the plain text file, mainOut, with midi instructions
+ *  newSong         - Algorithm to generate new midi information
+ *  findNoteIndex   - Finds and returns the index of the given note in the "dict" array
+ *  checkNote       - Checks what note a certain char should yield and returns that String
+ */
+
 import java.util.*;
 import java.io.*;
 import java.nio.file.Files;
@@ -5,30 +22,38 @@ import java.nio.file.Paths;
 
 public class Translate
 {
+    // CLASS VARIABLES
+    
+    // // Constants
     private static final String MIDI_STRING1 = "BA 1 CR ";
     private static final String MIDI_STRING2 = " TR 0 CH 1 NT ";
-    private static String MIDI_STRING3 = " 2";
+    private static final String MIDI_STRING3 = " 2";
+    private static final String [] dict = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+   
+    // // Variables
     private static int midiTimeCounter = 0;
     private static int midiNoteCounter = 0;
     private static Boolean first = true;
     private static String prevNote = "";
-    public final static String [] dict = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"};
+    
 
     public static void main(String [] args) {
 
         long startTime = System.nanoTime();
 
+        //Run newSong with text from input.txt
         String str = readFile("input.txt");
-        System.out.println(str);
-        // inverseCtrPt(str);
-        // chords(str);
         newSong(str);
 
         long endTime = System.nanoTime();
+
+        //Calculate time it took for the program to run
         long decSec = (endTime - startTime) / (100000000);
         System.out.println("Program Took About "+ (decSec/10) + "." + (decSec%10) + " secs"); 
     }
 
+    // Return string of the input text from the file, fileName
+    // Note: If nothing can be read from the file an empty string is returned
     public static String readFile(String fileName) {
         String content = "";
         try {
@@ -39,131 +64,100 @@ public class Translate
         return content;
     }
 
-    public static void chords(String str){
-        LinkedList <String> chordStrings = new LinkedList<>();
-        str = str.toLowerCase();
-        StringTokenizer tokens = new StringTokenizer(str);
-        while(tokens.hasMoreTokens()){
-            // System.out.print(tokens.nextToken()+"\n");
-            chordStrings.add(tokens.nextToken());
-        }
-        for(String toke: chordStrings){
-            LinkedList <String> tokenChord = new LinkedList<>();
-            char arr[] = toke.toCharArray();
-            for(char note: arr){
-                tokenChord.add(checkNote(note));
-            }
-            for(String note: tokenChord) {
-                try {
-                    appendchords(note);
-                }
-                catch (IOException io) {
-                    System.out.println("Couldn't append chord\n");
-                }
-            }
-            midiTimeCounter+= 2;
-            System.out.println(tokenChord);
-        }
-        System.out.println("Linked list of tokens: " + chordStrings);
-    }
-
-    public static void appendchords(String str) throws IOException {
-        if(str.equals("")){
-            return;
-        }
-        BufferedWriter writer = new BufferedWriter(new FileWriter("mainOut",true));
-        // char arr[] = str.toCharArray();
-        if(first==true) {
-            first = false;
-            writer.append('\n' + MIDI_STRING1 + midiTimeCounter + MIDI_STRING2 + str + MIDI_STRING3 + " von=100\n");
-            
-        }
-        else {
-            if(str.equals(prevNote)) {
-                writer.append(MIDI_STRING1 + midiTimeCounter + MIDI_STRING2 + str + "\'" + MIDI_STRING3 + '\n');
-            }
-            else{
-                writer.append(MIDI_STRING1 + midiTimeCounter + MIDI_STRING2 + str + MIDI_STRING3 + '\n');
-            }
-        }
-        writer.close();
-    }
-    
-    public static void inverseCtrPt(String str){
-        str = str.toLowerCase();
-        str = str.replaceAll("\\s+","");
-        char arr[] = str.toCharArray();
-        for(char c: arr) {
-            int newCharNum = 122-(((int) c)-97);
-            char d = (char) newCharNum;
-            try {
-                String tmp = checkNote(c);
-                append2midifile(tmp);
-                prevNote = tmp;
-                tmp = checkNote(d);
-                append2midifile(tmp);
-                prevNote = tmp;
-            }
-            catch (IOException io) {
-                System.out.println("Couldn't write to file.");
-            }
-            prevNote=checkNote(c);
-        }
-    }
-
+    //Appends a new note to the file, mainOut, from the given string str.
     public static void append2midifile(String str) throws IOException{
+
+        //Case if empty string is passed. Don't do anything
         if(str.equals("")){
             return;
         }
+
+        //Open buffered writer to write to the file, mainOut
         BufferedWriter writer = new BufferedWriter(new FileWriter("mainOut",true));
+
+        //Case if this is the first note to write. Must turn velocity on
         if(first==true) {
             first = false;
             writer.append('\n' + MIDI_STRING1 + midiTimeCounter + MIDI_STRING2 + str + MIDI_STRING3 + " von=100\n");
         }
+
+        //Case if it's any other note
         else {
+            //Case if the note equals previous note. Plays an octave up, so there is no overlap
             if(str.equals(prevNote)) {
                 writer.append(MIDI_STRING1 + midiTimeCounter + MIDI_STRING2 + str + "\'" + MIDI_STRING3 + '\n');
             }
+            //Case if it's a unique note
             else{
                 writer.append(MIDI_STRING1 + midiTimeCounter + MIDI_STRING2 + str + MIDI_STRING3 + '\n');
             }
         }
+
+        //Close the writer
         writer.close();
+
+        //Increment the note counter
         midiNoteCounter++;
+
+        //Increment time counter every other note, so there can be two notes played at the same time
         if(midiNoteCounter % 2 == 0) {
             midiTimeCounter ++;
         }
     }
 
+    // Makes a new complete midi song from the input string str.
+    // Note: Argument should be the complete string from the given text file
     public static void newSong(String str) {
-        str = str.toLowerCase();
-        str = str.replaceAll("[^A-Za-z]+","");
-        System.out.println(str);
-        char arr[] = str.toCharArray();
+
+        // Make the string pretty
+        str = str.toLowerCase();               // All lower case characters
+        str = str.replaceAll("[^A-Za-z]+",""); // Keep only alphabetic characters
+        char arr[] = str.toCharArray();        // Put each character into a char array so it can be traversed
+
+        // Traverse each character in the string
+        // Note: In each instance of this loop, two notes will be appended to be played at the same time in the midi track
         for(char c: arr) {
+            // Try to append midi file. Catch IOException
             try {
+                // Save what note this character corresponds to
                 String tmp = checkNote(c);
+                // Append the note
                 append2midifile(tmp);
+                // Save this note as prevNote
                 prevNote = tmp;
+
+                // Save which note the previous note is
                 int noteIndex = findNoteIndex(tmp);
+
+                // Random cases to choose what interval should be played for harmony
                 if(Math.random()<.2) {
+                    // Appends a note 7 semitones up from the first
                     append2midifile(dict[(noteIndex+7)%12]);
+                    // Saves this note as prevNote
                     prevNote = dict[(noteIndex+7)%12];
                 }
                 else if (Math.random()<.4){
+                    // Appends a note 10 semitones up from the first
                     append2midifile(dict[(noteIndex+10)%12]);
+                    // Saves this note as prevNote
                     prevNote = dict[(noteIndex+10)%12];
                 }
                 else if (Math.random()<.6) {
+                    // Appends a note 5 semitones up from the first
                     append2midifile(dict[(noteIndex+5)%12]);
+                    // Saves this note as prevNote
                     prevNote = dict[(noteIndex+5)%12];
                 }
                 else if (Math.random()<.8) {
+                    // Appends a note 6 semitones up from the first
                     append2midifile(dict[(noteIndex+6)%12]);
+                    // Saves this note as prevNote
                     prevNote = dict[(noteIndex+6)%12];
                 }
                 else if (Math.random()<1) {
+                    // Appends a note 4 semitones up from the first
                     append2midifile(dict[(noteIndex+4)%12]);
+                    // Saves this note as prevNote
                     prevNote = dict[(noteIndex+4)%12];
                 }
                 
@@ -171,18 +165,23 @@ public class Translate
             catch (IOException io) {
                 System.out.println("Couldn't write to file.");
             }
-            prevNote=checkNote(c);
         }
     }
 
+    // Find the index of the given argument note in the "dict" array
+    // Precondition: "note" must be a valid note string that exists in dict or 0 will be returned
     public static int findNoteIndex(String note){
+        //Traverse the entire dict array
         for(int i = 0; i < dict.length; i++) {
+            //Check if the given note matches the note in dict
             if(note.equals(dict[i]))
                 return i;
         }
         return 0;
     }
 
+    // Returns a String representing a note in the western scale from a given alphabetic character
+    // Precondition: Entered char c must be a valid alphabetic char or an empty string will be returned
     public static String checkNote(char c){
         if(c=='a' || c=='m' || c=='n' || c=='z') {
             return "C";
